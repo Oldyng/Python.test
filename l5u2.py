9984f2f9e88510062f99aeb3ccd7676432e1bb0c
Sergey <162873784+Oldyng@users.noreply.github.com>
2024-03-20 18:49:36 +0700

    Update l5u2.py

--- l5u2.py
+++ l5u2.py
@@ -1,16 +1,36 @@
 print("Введите слово маленькими латинскмими буквами")
 word = str(input())
 
+v = 0
+c = 0
 a = 0
-b = 0
+e = 0
+i = 0
+o = 0
+u = 0
 
 for letter in word:
     if letter in "aeiou":
-        a += 1
+        v += 1
     elif letter in "bcdfghjklmnpqrstvwxyz":
-        b += 1
-else:
-    print(False)
+        c += 1
 
-print("Гласных букв:", a)
-print("Согласных букв:", b)
+for letter in word:
+    if letter in "a":
+        a += 1
+    elif letter in "e":
+        e += 1
+    elif letter in "i":
+        i += 1
+    elif letter in "o":
+        o += 1
+    elif letter in "u":
+        u += 1
+
+print("Гласных букв:", v)
+print("Согласных букв:", c)
+
+if a + e + i + o + u == 0:
+    print(False)
+else:    
+    print(f"Количество букв a: {a};\nКоличество букв e: {e};\nКоличество букв i: {i};\nКоличество букв o: {o};\nКоличество букв u: {u}.")
